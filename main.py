5ed85bfb786da844e8ee6fcbee3a7c43b33a22b7
MimicTester1307 <excel.chukwu@ashesi.edu.gh>
2021-08-09 23:05:18 +0000

    implement notification logic

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 from flight_search import FlightSearch
 from data_manager import DataManager
 from flight_data import FlightData
+from notification_manager import NotificationManager
 
 sheet_data_manager = DataManager()
 flight_searcher = FlightSearch(sheet_data_manager)
@@ -11,8 +12,7 @@ flight_searcher = FlightSearch(sheet_data_manager)
 # Retrieving the Google Sheets
 sheet_data = sheet_data_manager.retrieve_sheets()
 cities = [city['city'] for city in sheet_data['prices']]    # creating a list of cities in the list
-prices = [city['lowestPrice'] for city in sheet_data['prices']]    #creating a list of all the prices
-# city_price = {(city, price) for city in cities for price in prices}
+
 row = 2
 
 # updating the IATA for each city in the
@@ -24,7 +24,15 @@ for city in cities:
 
 for city in sheet_data["prices"]:
     lowest_price = city["lowestPrice"]
-    minimum_price_data = flight_searcher.find_cheapest_flight(city=city, min_price=lowest_price)
+    minimum_price_data = flight_searcher.find_cheapest_flight(city=city['iataCode'], min_price=lowest_price)
+    print(minimum_price_data)
+    if minimum_price_data:
+        flight_data = FlightData(minimum_price_data)
+        formatted_details = flight_data.format_flight_details()
+        notifier = NotificationManager()
+        notifier.send_notification(formatted_details)
+
+
 
 
 
