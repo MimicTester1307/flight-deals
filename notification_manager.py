1ee11009cec1c582340744dd67e2228845858442
MimicTester1307 <excel.chukwu@ashesi.edu.gh>
2021-08-09 22:58:28 +0000

    implement method for sending notification

--- notification_manager.py
+++ notification_manager.py
@@ -1,3 +1,25 @@
-class NotificationManager:
+import os
+import requests
+from twilio.rest import Client
+from dotenv import load_dotenv
+
+# loading environment variables
+load_dotenv()
+TWILIO_SID = os.getenv('TWILIO_SID')
+TWILIO_AUTH = os.getenv('TWILIO_AUTH')
+TWILIO_NUMBER = os.getenv('TWILIO_NUMBER')
+MY_NUMBER = os.getenv('MY_NUMBER')
+
+class NotificationManager(Client):
     #This class is responsible for sending notifications with the deal flight details.
-    pass
+    def __init__(self):
+        self.client = Client(TWILIO_SID, TWILIO_AUTH)
+
+    def send_notification(self, message: str):
+        """Notifies the given number about the flight details"""
+        self.client.messages.create(
+            body=message,
+            from_=TWILIO_NUMBER,
+            to=MY_NUMBER
+        )
+
