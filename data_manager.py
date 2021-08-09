791ab4d5ef196aac4e5aa27a6fedfa2715147100
MimicTester1307 <excel.chukwu@ashesi.edu.gh>
2021-08-09 19:05:37 +0000

    implement retrieve_sheets method

--- data_manager.py
+++ data_manager.py
@@ -1,3 +1,21 @@
+import requests
+
+SHEETY_GET_API_KEY = "https://api.sheety.co/5f7bebf51e96e73ce7d6cb184e070fd4/flightDeals/prices"
+SHEETY_POST_API_KEY = "https://api.sheety.co/5f7bebf51e96e73ce7d6cb184e070fd4/flightDeals/prices"
+HEADER = {"Authorization": "Bearer AF79wN!NAHb4&YR"}
+
+
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
-    pass
+    # This class is responsible for talking to the Google Sheet.
+
+    def __init__(self):
+        pass
+
+    def retrieve_sheets(self):
+        response = requests.get(url=SHEETY_GET_API_KEY, headers=HEADER)
+        return response.json()
+
+
+    # def update_row(self):
+    #     SHEETY_PUT_API = "https://api.sheety.co/5f7bebf51e96e73ce7d6cb184e070fd4/flightDeals/prices/[Object ID]"
+    #
