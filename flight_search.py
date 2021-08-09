5e0a8cc6230d1b596eb965307f92a451682f6ccd
MimicTester1307 <excel.chukwu@ashesi.edu.gh>
2021-08-09 21:28:46 +0000

    implement find_cheapest_flight method

--- flight_search.py
+++ flight_search.py
@@ -1,5 +1,5 @@
 import requests
-from datetime import datetime
+from datetime import datetime,timedelta
 from data_manager import DataManager
 import os
 from dotenv import load_dotenv
@@ -15,6 +15,7 @@ TEQUILA_HEADER = {
     "apikey": TEQUILA_API_KEY,
 }
 
+CURRENT_CITY = "ACC,GH"
 
 class FlightSearch:
     # This class is responsible for talking to the Flight Search API.
@@ -27,7 +28,28 @@ class FlightSearch:
             "term": f"{city}",
             "location_types": "city"
         }
-        response = requests.get(url=TEQUILA_LOCATION_ENDPOINT, json=parameters, headers=TEQUILA_HEADER).json()
+        response = requests.get(url=TEQUILA_LOCATION_ENDPOINT, params=parameters, headers=TEQUILA_HEADER).json()
         iata_code = response["locations"][0]["code"]
 
         return iata_code
+
+    def find_cheapest_flight(self, city: str, min_price: float):
+        """Finds the cheapest flight to a particular City"""
+        minimum_price_data = None
+        current_date = datetime.now()
+        time_delta = timedelta(hours=4380)
+        next_six_months = (current_date + time_delta).strftime("%d/%m/%Y")
+        parameters = {
+            "fly_from": CURRENT_CITY,
+            "fly_to": city,
+            "date_from": current_date.strftime("%d/%m/%Y"),
+            "date_to": next_six_months,
+            "flight_type": "round"
+        }
+        responses = requests.get(url=TEQUILA_SEARCH_ENDPOINT, params=parameters, headers=TEQUILA_HEADER).json()
+        for response in responses["data"]:
+            price = response["price"]
+            if price < min_price:
+                minimum_price_data = response
+
+        return minimum_price_data
