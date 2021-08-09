b79eef5f617489d0628a3d737fc1ca454bd2070e
MimicTester1307 <excel.chukwu@ashesi.edu.gh>
2021-08-09 22:40:03 +0000

    implement flight data formatter

--- flight_data.py
+++ flight_data.py
@@ -1,3 +1,14 @@
+import pprint
+
+
 class FlightData:
-    #This class is responsible for structuring the flight data.
-    pass
+    # This class is responsible for structuring the flight data.
+    def __init__(self, flight_details: dict):
+        self.flight_details = flight_details
+
+    def format_flight_details(self):
+        price = self.flight_details["price"]
+        from_location = f"{self.flight_details['cityFrom']}-{self.flight_details['flyFrom']}"
+        to_location = f"{self.flight_details['cityTo']}-{self.flight_details['flyTo']}"
+
+        formatted_message = f"Low price alert! Only ${price} ro fly from {from_location} to {to_location}"
