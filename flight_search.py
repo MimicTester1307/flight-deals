f6cc9370dd2a422af916f61afb95e98f93557a80
MimicTester1307 <excel.chukwu@ashesi.edu.gh>
2021-08-09 19:40:15 +0000

    implement environment variables

--- flight_search.py
+++ flight_search.py
@@ -1,3 +1,36 @@
+import requests
+from datetime import datetime
+from data_manager import DataManager
+import os
+from dotenv import load_dotenv
+
+# Loading environment variables
+load_dotenv()
+
+
+TEQUILA_SEARCH_ENDPOINT = "tequila-api.kiwi.com/v2/search"
+TEQUILA_API_KEY = os.getenv("TEQUILA_API_KEY")
+TEQUILA_LOCATION_ENDPOINT = "https://tequila-api.kiwi.com/locations/query"
+TEQUILA_HEADER = {
+    "apikey": TEQUILA_API_KEY,
+}
+
+
 class FlightSearch:
-    #This class is responsible for talking to the Flight Search API.
-    pass
+    # This class is responsible for talking to the Flight Search API.
+    def __init__(self, data: DataManager):
+        self.data = data
+
+    def retrieve_iata(self):
+        sheet_data = self.data.retrieve_sheets()
+        cities = [city['city'] for city in sheet_data['prices']]
+
+        for city in cities:
+            parameters = {
+                "term": f"{city}",
+                "location_types": "city"
+            }
+            response = requests.get(url=TEQUILA_LOCATION_ENDPOINT, json=parameters, headers=TEQUILA_HEADER).json()
+            iata_code = response["locations"][0]["code"]
+
+
